11e26a2160e24d8c916f6df657bc70d8a394c100
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-11 01:03:07 +0600

    Parser

--- project-1/dist/app/app.js
+++ project-1/dist/app/app.js
@@ -1,8 +1,20 @@
 import express, {} from "express";
 const app = express();
+// parse application/json
+app.use(express.json());
 const port = 3000;
+//parse json request body
+app.use(express.json());
+app.use(express.text()); // parse text/plain request body
+app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded request body
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "POST request received!",
+    });
 });
 export default app;
 //# sourceMappingURL=app.js.map
--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -1,9 +1,24 @@
 import express, { type Request, type Response } from "express";
 const app = express();
+// parse application/json
+app.use(express.json());
+
 const port = 3000;
+
+//parse json request body
+app.use(express.json());
+app.use(express.text()); // parse text/plain request body
+app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded request body
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.json({
+    message: "POST request received!",
+  })
 });
 
 export default app;
