28782b242cb40da7884898e4016098cac354b308
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-10 20:14:41 +0600

    Streaming and buffer

--- stream-buffer.js
+++ stream-buffer.js
@@ -1,14 +1,25 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 //creating a server using raw node.js
 const server = http.createServer();
 
 //listener
-server.on('request', (req, res) => {
-    console.log(req);
-    res.end('Hello, World!');
-})
+server.on("request", (req, res) => {
+  console.log(req.url, req.method);
+  if (req.url === "/read-file" && req.method === "GET") {
+    const readStream = fs.createReadStream(process.cwd() + "/read.txt");
+
+    readStream.on("data", (chunk) => {
+      res.write(chunk);
+    });
+
+    readStream.on("end", () => {
+      res.end();
+    });
+  }
+});
 
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
